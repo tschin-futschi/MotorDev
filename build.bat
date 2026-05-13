9f128eb6980700b2b45a319b61c44ad44898e172
tschin-futschi <135839758+tschin-futschi@users.noreply.github.com>
2026-05-13 20:05:12 +0800

    修改build.bat, 即插即用

--- build.bat
+++ build.bat
@@ -1,15 +1,17 @@
 @echo off
-set MINGW_PATH=D:\Qt\Tools\mingw1310_64\bin
-set BUILD_DIR=%~dp0build_make_qt
-set PATH=%MINGW_PATH%;%PATH%
+ 
+set QT_ROOT=C:\Qt\6.11.0\mingw_64
+set MINGW_ROOT=C:\Qt\Tools\mingw1310_64
+set CMAKE_ROOT=C:\Qt\Tools\CMake_64
+set PATH=%MINGW_ROOT%\bin;%QT_ROOT%\bin;%CMAKE_ROOT%\bin;%PATH%
 
-cd /d "%BUILD_DIR%"
-"%MINGW_PATH%\mingw32-make.exe" -j8
-if %ERRORLEVEL% EQU 0 (
-    echo.
-    echo BUILD SUCCESS
-) else (
-    echo.
-    echo BUILD FAILED
-)
-pause
+"%CMAKE_ROOT%\bin\cmake.exe" ^
+  -S . ^
+  -B build_make_qt ^
+  -G "MinGW Makefiles" ^
+  -DCMAKE_PREFIX_PATH="%QT_ROOT%" ^
+  -DQt6_DIR="%QT_ROOT%\lib\cmake\Qt6" ^
+  -DCMAKE_CXX_COMPILER="%MINGW_ROOT%\bin\g++.exe" ^
+  -DCMAKE_MAKE_PROGRAM="%MINGW_ROOT%\bin\mingw32-make.exe"
+ 
+"%MINGW_ROOT%\bin\mingw32-make.exe" -C build_make_qt -j4
